b04a26960cb5e08e182847a96d05d8d665519ea9
GitAyan <30700046+GitAyan@users.noreply.github.com>
2017-08-25 11:40:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,12 @@ console.log('Loaded!');
 //Making an alert in Javascript
 //alert("Oh Main.js Alert!");
 
-//Changing the document loading
 
+
+
+
+
+//Changing the document loading
 var el=document.getElementById('bigtext');
 el.innerHTML="Hello, Main.js changed the text to this while loading";
 counter=0;
@@ -29,8 +33,15 @@ img.onclick=function(){
   var interval=setInterval(moveRight,50);
 }
 
-//Chapter 16 API
 
+
+
+
+
+
+
+
+//Chapter 16 API
 var c=0;
 var button1=document.getElementById('button1');
 button1.onclick=function()
@@ -54,16 +65,20 @@ request.open('GET','http://endecipher.imad.hasura-app.io/counter',true);
 request.send(null);
 }//button onlcick
 
-//JSON
 
+
+
+
+
+
+
+
+//JSON
 var namebutton=document.getElementById('namebutton');
 namebutton.onclick=function(){
-
 var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
-
 if (request.readyState===XMLHttpRequest.DONE){
-
    if(request.status===200){
       var list='';
       var names=request.responseText;
@@ -74,17 +89,58 @@ if (request.readyState===XMLHttpRequest.DONE){
       }
      ul.innerHTML=list;
    }
-
-
 }
-
-
 }//onreadystatechange ends
 var name=document.getElementById('name').value;
 request.open('GET','http://endecipher.imad.hasura-app.io/submit?name='+name,true);
 request.send(null);
 
 }//namebutton onclick ends
+
+
+
+
+
+
+
+
+
+var namebutton=document.getElementById('uisubmit');
+uisubmit.onclick=function(){
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+if (request.readyState===XMLHttpRequest.DONE){
+   if(request.status===200){
+     alert("User Logged In Successfully!");
+      }else if(request.status===403){
+     alert("Forbidden! User Credentials Invalid");
+      }else if(request.status===500){
+     alert("Server Unknown error!");
+      }
+     else{
+         alert("Unknown error");
+     }
+     
+     
+     
+   }
+}
+}//onreadystatechange ends
+var name=document.getElementById('uiusername').value;
+var password=document.getElementById('uipassword').value;
+request.open('POST','http://endecipher.imad.hasura-app.io/login',true);
+request.send(JSON.stringify({"username":name, "password":password}));
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
