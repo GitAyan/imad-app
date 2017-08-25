cdcf5fb6f071195205b535ac5c533fe9150b1267
GitAyan <30700046+GitAyan@users.noreply.github.com>
2017-08-25 13:42:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ return ['pbkdf',10000,salt,hash].join('$');
 
 app.get('/password/:input', function(req,res){
     var hashedString=hash(req.params.input,'this-salt-value');
-    res.send(hashedString.length().toString());
+    res.send(length(hashedString).toString());
 });
 
 app.post('/create-user',function(req,res){
