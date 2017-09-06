b304ded6f248d24b01bb00d5f0012346dab0723d
GitAyan <30700046+GitAyan@users.noreply.github.com>
2017-09-06 11:55:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -283,8 +283,16 @@ var HTMLTemplate=`
 return HTMLTemplate;
 }
 
-app.get('/get-articles',function (req,res){
-  res.send(JSON.stringify(article2));
+app.get('/get-articles', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
 });
 
 
